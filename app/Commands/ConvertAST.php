c4233c92c867890b99e4895df232222ed9ecbac6
Wouter van den Brink <git@woutervdb.com>
2025-10-21 19:48:32 +0200

    Add AST to XML converter

---
+++ app/Commands/ConvertAST.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Commands;
+
+use App\Parser\Visitors\ASTToXMLConvertingVisitor;
+use App\Parser\Visitors\NodeFeatureConstraintDetectingVisitor;
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+use PhpParser\NodeTraverser;
+use PhpParser\ParserFactory;
+use PhpParser\PhpVersion;
+
+class ConvertAST extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ast:convert
+                            {--parser=latest : The PHP version to use when parsing, or \'latest\' for the latest supported version.}
+                            {--O|overwrite : Overwrite the output file if it already exists.}
+                            {input : The input file to parse, e.g. test.php}
+                            {output : The output file to write to, e.g. test.ast.xml}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Parse a PHP file and convert the AST to XML';
+
+    private function ensureInput(string $input): bool
+    {
+        if (! file_exists($input) || ! is_readable($input)) {
+            $this->error('Input file '.$input.' does not exist or is not readable');
+
+            return false;
+        }
+
+        return true;
+    }
+
+    private function ensureOutput(string $output): bool
+    {
+        if (file_exists($output)) {
+            if (! $this->option('overwrite') && ! $this->confirm('Output file '.$output.' exists. Overwrite?')) {
+                return false;
+            }
+        }
+
+        if (! touch($output) || ! is_writable($output)) {
+            $this->error('Output file '.$output.' is not writable');
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(ParserFactory $factory, NodeTraverser $traverser): void
+    {
+        $input = $this->argument('input');
+        $output = $this->argument('output');
+
+        if (! $this->ensureInput($input) || ! $this->ensureOutput($output)) {
+            return;
+        }
+
+        $parser = $this->option('parser');
+        $parser = $factory->createForVersion($parser === 'latest' ? PhpVersion::getNewestSupported() : PhpVersion::fromString($parser));
+
+        $traverser->addVisitor(new NodeFeatureConstraintDetectingVisitor);
+        $traverser->addVisitor($xml = new ASTToXMLConvertingVisitor);
+
+        $stmts = $parser->parse(file_get_contents($input)) ?? [];
+
+        $traverser->traverse($stmts);
+
+        $out = $xml->document->saveXML();
+
+        file_put_contents($output, $out);
+
+        $this->info('Written XML output to '.$output);
+    }
+
+    /**
+     * Define the command's schedule.
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
--- app/Parser/Visitors/ASTToXMLConvertingVisitor.php
+++ app/Parser/Visitors/ASTToXMLConvertingVisitor.php
@@ -2,6 +2,94 @@
 
 namespace App\Parser\Visitors;
 
-class ASTToXMLConvertingVisitor {
+use App\Language\PhpVersionConstraint;
+use DOMDocument;
+use DOMException;
+use DOMNode;
+use LogicException;
+use Override;
+use PhpParser\Node;
+use PhpParser\NodeVisitorAbstract;
 
+class ASTToXMLConvertingVisitor extends NodeVisitorAbstract
+{
+    public private(set) DOMDocument $document;
+
+    private DOMNode $cursor;
+
+    /**
+     * @throws DOMException
+     */
+    #[Override]
+    public function beforeTraverse(array $nodes): null
+    {
+        $this->document = new DOMDocument;
+        $this->document->formatOutput = true;
+
+        $cursor = $this->document->createElement('ast');
+        $this->document->appendChild($cursor);
+
+        $this->cursor = $cursor;
+
+        return null;
+    }
+
+    private static function convertAttributeValueToString(mixed $value): string
+    {
+        if ($value === null) {
+            return 'null';
+        }
+        if (is_scalar($value)) {
+            return (string) $value;
+        }
+        if (is_array($value)) {
+            return '['.implode(', ', array_map(self::convertAttributeValueToString(...), $value)).']';
+        }
+        if (is_object($value) && method_exists($value, '__toString')) {
+            return $value->__toString();
+        }
+
+        return '__not_a_string__';
+    }
+
+    /**
+     * @throws DOMException
+     */
+    #[Override]
+    public function enterNode(Node $node): null
+    {
+        $child = $this->document->createElement($node->getType());
+
+        foreach ($node->getAttributes() as $key => $value) {
+            if (preg_match('/(start|end)(Line|TokenPos|FilePos)/', $key)) {
+                continue;
+            }
+
+            if ($key === 'constraint' && $value instanceof PhpVersionConstraint) {
+                $child->setAttribute('constraintMin', $value->min?->toVersionString() ?? 'none');
+                $child->setAttribute('constraintMax', $value->max?->toVersionString() ?? 'none');
+
+                continue;
+            }
+
+            $child->setAttribute($key, self::convertAttributeValueToString($value));
+        }
+
+        $this->cursor->appendChild($child);
+        $this->cursor = $child;
+
+        return null;
+    }
+
+    #[Override]
+    public function leaveNode(Node $node): null
+    {
+        if ($this->cursor->parentNode === null) {
+            throw new LogicException('This should never happen!');
+        }
+
+        $this->cursor = $this->cursor->parentNode;
+
+        return null;
+    }
 }
