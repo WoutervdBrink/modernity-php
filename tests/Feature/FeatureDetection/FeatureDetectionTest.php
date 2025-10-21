880c8f26045f4c536e9cf7b0f252a5780f70527b
Wouter van den Brink <git@woutervdb.com>
2025-10-21 19:48:00 +0200

    Add helpful error messages to feature detection test

--- tests/Feature/FeatureDetection/FeatureDetectionTest.php
+++ tests/Feature/FeatureDetection/FeatureDetectionTest.php
@@ -32,8 +32,8 @@ describe('Feature detection', function () {
 
         $constraint = $minMax->constraint;
 
-        expect($constraint->min)->toBe($example->minVersion)
-            ->and($constraint->max)->toBe($example->maxVersion);
+        expect($constraint->min)->toBe($example->minVersion, 'Expected minimum version '.($example->minVersion?->toVersionString() ?? 'none'))
+            ->and($constraint->max)->toBe($example->maxVersion, 'Expected maximum version '.($example->maxVersion?->toVersionString() ?? 'none'));
     })->with(/** @return Generator<Example> */ function (): Generator {
         $iterator = new DirectoryIterator(__DIR__.'/../../Fixtures/FeatureDetection');
 
