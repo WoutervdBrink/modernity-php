1e8d89b6130f0e0484fda93a0db13bf7917e645f
Wouter van den Brink <git@woutervdb.com>
2025-12-18 11:44:12 +0100

    Add cURL removed constants to catalogue and test suite

---
+++ tests/Fixtures/FeatureDetection/5.6/curl_removed_constants.php
@@ -0,0 +1,54 @@
+#Test
+Description: The CURLOPT_CLOSEPOLICY constant was removed in PHP 5.6.
+Parser: 5.6
+Max: 5.5
+
+<?php
+echo CURLOPT_CLOSEPOLICY;
+?>
+
+#Test
+Description: The CURLCLOSEPOLICY_CALLBACK constant was removed in PHP 5.6.
+Parser: 5.6
+Max: 5.5
+
+<?php
+echo  CURLCLOSEPOLICY_CALLBACK;
+?>
+
+#Test
+Description: The CURLCLOSEPOLICY_LEAST_RECENTLY_USED constant was removed in PHP 5.6.
+Parser: 5.6
+Max: 5.5
+
+<?php
+echo CURLCLOSEPOLICY_LEAST_RECENTLY_USED;
+?>
+
+#Test
+Description: The CURLCLOSEPOLICY_LEAST_TRAFFIC constant was removed in PHP 5.6.
+Parser: 5.6
+Max: 5.5
+
+<?php
+echo CURLCLOSEPOLICY_LEAST_TRAFFIC;
+?>
+
+#Test
+Description: The CURLCLOSEPOLICY_OLDEST constant was removed in PHP 5.6.
+Parser: 5.6
+Max: 5.5
+
+<?php
+echo CURLCLOSEPOLICY_OLDEST;
+?>
+
+#Test
+Description: The CURLCLOSEPOLICY_SLOWEST constant was removed in PHP 5.6.
+Parser: 5.6
+Max: 5.5
+
+<?php
+echo CURLCLOSEPOLICY_SLOWEST;
+?>
+
